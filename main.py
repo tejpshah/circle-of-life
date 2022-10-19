4662cf493cbdb50dc0ea588cd7a53b180a4e442e
Srinandini Marpaka <srinandinim@hotmail.com>
2022-10-19 16:40:53 -0400

    add nodes parameter to simulations

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from game.game import Game
 
-def simulation_statistics_agent1(num_simulations):
+def simulation_statistics_agent1(num_simulations, nodes=50):
     """
     run simulation n times and get statistics on success
     """
     agent_success = []
     for _ in range(num_simulations):
-        game = Game(50)
+        game = Game(nodes)
         game_success = game.run_agent_1()
 
         # agent caught the prey = 1, predator caught the agent = 0
@@ -19,13 +19,13 @@ def simulation_statistics_agent1(num_simulations):
         f"Agent1: Wins: {wins}\tLosses: {losses}\tSuccess Rate: {round(success*100,2)}%")
     return round(success*100, 2)
 
-def simulation_statistics_agent2(num_simulations):
+def simulation_statistics_agent2(num_simulations, nodes=50):
     """
     run simulation n times and get statistics on success
     """
     agent_success = []
     for _ in range(num_simulations):
-        game = Game(50)
+        game = Game(nodes)
         game_success = game.run_agent_2()
 
         # agent caught the prey = 1, predator caught the agent = 0
