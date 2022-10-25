dc66dceb348e087f5054842d1436503319023ec0
Srinandini Marpaka <srinandinim@hotmail.com>
2022-10-25 19:48:20 -0400

    updated partial prey visualization

--- main.py
+++ main.py
@@ -101,7 +101,5 @@ if __name__ == "__main__":
 
     # labreport_simulation_statistics_agent3()
     # labreport_simulation_statistics_agent4()
-    # simulation_statistics.visualize(
-    #     "data/", "simulation_statistics_partial-prey.json")
-
-    simulation_statistics.agent4(1, 10)
+    simulation_statistics.visualize(
+        "data/", "simulation_statistics_partial-prey.json")
